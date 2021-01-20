0f3b50aff7c19111bb6867ac8746531a8132fa66
Francis Langit <francis.villanueva.langit@gmail.com>
2021-01-20 18:43:01 +0800

    Fix form resubmit upon refresh bug
    The form no longer resubmits input text upon refresh of page. Bug was
    fixed by utilizing Django's messages framework.

--- cypher_app/views.py
+++ cypher_app/views.py
@@ -1,22 +1,46 @@
-from django.shortcuts import render 
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from .forms import InputTextForm
 
+# Import cipher algorithms from ciphers package.
+from cypher_app.ciphers.binary import Binary
+from cypher_app.ciphers.caesar_cipher import CaesarCipher
+from cypher_app.ciphers.morse_code import MorseCode
 from cypher_app.ciphers.pig_latin import PigLatin
 
+# def index(request):
+# 	"""Home page for Cypher."""
+# 	if request.method == 'POST':
+# 		form = InputTextForm(request.POST)
+# 		if form.is_valid():
+# 			input_text = form.cleaned_data['input_text']
+# 			ciphered_text = MorseCode(input_text).cipher()
+# 			return render(request, 'cypher_app/index.html', {
+# 					'form': form,
+# 					'ciphered_text': ciphered_text,
+# 				},
+# 			)
+# 	else:
+# 		form = InputTextForm()
+
+# 	return render(request, 'cypher_app/index.html', {'form': form})
+
+
 def index(request):
-	"""Home page for Cypher."""
 	if request.method == 'POST':
 		form = InputTextForm(request.POST)
+
 		if form.is_valid():
 			input_text = form.cleaned_data['input_text']
-			ciphered_text = PigLatin(input_text).cipher()
-			return render(request, 'cypher_app/index.html', {
-					'form': form,
-					'ciphered_text': ciphered_text,
-				},
-			)
+			ciphered_text = Binary(input_text).cipher()
+
+			# return render(request, 'cypher_app/index.html', {
+			# 		'form': form,
+			# 		'ciphered_text': ciphered_text,
+			# 	})
+			messages.add_message(request, messages.INFO, ciphered_text)
+			return redirect('/',)
 	else:
 		form = InputTextForm()
-
 	return render(request, 'cypher_app/index.html', {'form': form})
