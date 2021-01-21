97c90c539deacda5cf2b71a2f01eab7705282bed
Francis Langit <francis.villanueva.langit@gmail.com>
2021-01-21 10:37:16 +0800

    Create decipher method for Binary class

--- cypher_app/ciphers/binary.py
+++ cypher_app/ciphers/binary.py
@@ -1,8 +1,8 @@
 class Binary:
-	"""Represents text to be ciphered into Binary.
+	"""Represents text to be ciphered into Binary and vice versa.
 
 	Attributes:
-		text (str): Text to cipher.
+		text (str): Text to cipher/decipher.
 	"""
 
 
@@ -21,3 +21,20 @@ class Binary:
 		    str: Text ciphered into Binary
 		"""
 		return ' '.join(format(ord(char), 'b') for char in self.text)
+
+
+	def decipher(self):
+		"""Deciphers a string composed of binary values into ASCII text.
+
+		Adapted from kite.com's "How to convert binary to string in Python:" (
+		https://www.kite.com/python/answers/how-to-convert-binary-to-string-in
+		-python).
+
+		Args: 
+			None
+
+		Returns:
+			str: Text deciphered from Binary to ASCII text.
+		"""
+		return ''.join(
+			[chr(int(binary, 2)) for binary in binary_string.split(" ")])
