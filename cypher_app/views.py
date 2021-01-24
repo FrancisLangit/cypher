06f4b4861bb906c8ea788553614c6fd9a3d9cccf
Francis Langit <francis.villanueva.langit@gmail.com>
2021-01-24 16:51:33 +0800

    Add docstrings and comments to views.py of cypher_app

--- cypher_app/views.py
+++ cypher_app/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 
 from .forms import CaesarCipherForm, OtherCiphersForm
 
-# Import CaesarCipher class from local ciphers package.
+# Import modules from local ciphers package.
 from cypher_app.ciphers import (
 	binary,
 	caesar_cipher,
@@ -11,7 +11,7 @@ from cypher_app.ciphers import (
 	pig_latin,
 )
 
-# Make dictionary pairing OtherCiphersForm cipher choices to their modules.
+# Dictionary pairing OtherCiphersForm cipher choices to their modules.
 CIPHER_DICT = {
 	"binary": binary.Binary,
 	"morse_code": morse_code.MorseCode,
@@ -20,16 +20,28 @@ CIPHER_DICT = {
 
 
 def index(request):
+	"""Index view of the website. 
+
+	Defaults to redirecting the user to the app page of the website.
+	"""
 	return redirect('/app')
 
 
 def app(request):
+	"""App view of the website.
+
+	Where the user can turn their text into a Caesar Cipher.
+	"""
 	if request.method == 'POST':
 		form = CaesarCipherForm(request.POST)
 		if form.is_valid():
+			# Create CaesarCipher object with form data.
 			ciphered_text = caesar_cipher.CaesarCipher(
 				form.cleaned_data['text'], 
-				form.cleaned_data['key'],).cipher()
+				form.cleaned_data['key'],
+			).cipher()
+
+			# Append ciphered text to messages.
 			messages.add_message(request, messages.INFO, ciphered_text)
 		return redirect('/app')
 	else:
@@ -38,15 +50,15 @@ def app(request):
 
 
 def others(request):
+	"""Others view of the website.
+
+	User can access the other ciphering algorithms here.
+	"""
 	if request.method == 'POST':
 		form = OtherCiphersForm(request.POST)
 		if form.is_valid():
-			cipher_choice = form.cleaned_data['cipher_choice']
-			text = form.cleaned_data['text']
-
-			cipher_module = CIPHER_DICT[cipher_choice]
-			ciphered_text = cipher_module(text).cipher()
-
+			cipher_class = CIPHER_DICT[form.cleaned_data['cipher_choice']]
+			ciphered_text = cipher_class(form.cleaned_data['text']).cipher()
 			messages.add_message(request, messages.INFO, ciphered_text)
 		return redirect('app/others')
 	else:
