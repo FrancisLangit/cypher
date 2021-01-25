a34386c8eef60542da88bbf67e0d523e9d1ae0a8
Francis Langit <francis.villanueva.langit@gmail.com>
2021-01-25 11:47:24 +0800

    Add key IntegerField to CipherTextForm and delete CaesarCipherForm

--- cypher_app/forms.py
+++ cypher_app/forms.py
@@ -1,8 +1,5 @@
 from django import forms
 
-class CaesarCipherForm(forms.Form):
-	text = forms.CharField(label='Text', max_length=1000)
-	key = forms.IntegerField(min_value=0, max_value=26)
-
 class CipherTextForm(forms.Form):
 	text = forms.CharField(label='Text', max_length=1000)
+	key = forms.IntegerField(min_value=0, max_value=26)
