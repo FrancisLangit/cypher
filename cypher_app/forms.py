f9d76c4d801c655528c4a14698e36a11eae7f07d
Francis Langit <francis.villanueva.langit@gmail.com>
2021-01-25 12:00:49 +0800

    Make "key" IntegerField not required

--- cypher_app/forms.py
+++ cypher_app/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 class CipherTextForm(forms.Form):
 	text = forms.CharField(label='Text', max_length=1000)
-	key = forms.IntegerField(label='Key', min_value=0, max_value=26)
+	key = forms.IntegerField(
+		label='Key', min_value=0, max_value=26, required=False)
