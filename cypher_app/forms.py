2f30d105da3fed93cf202e7731b8198dfc9092c4
Francis Langit <francis.villanueva.langit@gmail.com>
2021-01-26 09:54:29 +0800

    Add operation ChoiceField to CipherTextForm

--- cypher_app/forms.py
+++ cypher_app/forms.py
@@ -5,3 +5,10 @@ class CipherTextForm(forms.Form):
 	text = forms.CharField(label='Text', max_length=1000)
 	key = forms.IntegerField(
 		label='Key', min_value=0, max_value=26, required=False)
+	operation = forms.ChoiceField(
+		label='Cipher or Decipher',
+		choices=(
+			('cipher', "Cipher"),
+			('decipher', "Decipher"),
+		))
+
