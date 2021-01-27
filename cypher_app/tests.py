a8ff3f353541f65421f851f4d891dd1f958e204f
Francis Langit <francis.villanueva.langit@gmail.com>
2021-01-27 10:04:32 +0800

    Write test cases for cipher and decipher algorithms

--- cypher_app/tests.py
+++ cypher_app/tests.py
@@ -11,20 +11,46 @@ class CiphersTestCase(TestCase):
 
 
 	def setUp(self):
-		self.cipher_text = "The quick brown fox jumps over the lazy dog."
+		self.short_text = "Slim!"
+		self.long_text = "The quick brown fox jumps over the lazy dog."
+		self.caesar_key = 2
 
-		self.cipher_binary = binary.Binary(self.cipher_text)
-		self.cipher_caesar = caesar_cipher.CaesarCipher(self.cipher_text, 2)
-		self.cipher_morse = morse_code.MorseCode(self.cipher_text)
-		self.cipher_pig = pig_latin.PigLatin(self.cipher_text)
+		self.cipher_binary = binary.Binary(self.short_text)
+		self.cipher_caesar = caesar_cipher.CaesarCipher(
+			self.long_text, self.caesar_key)
+		self.cipher_morse = morse_code.MorseCode(self.short_text)
+		self.cipher_pig = pig_latin.PigLatin(self.long_text)
 
 
 	def test_cipher(self):
-		self.assertEqual(self.cipher_morse.cipher(),
-			"- .... . ....... --.- ..- .. -.-. -.- ....... -... .-. --- .-- -." + 
-			"....... ..-. --- -..- ....... .--- ..- -- .--. ... ....... --- ..." +
-			"- . .-. ....... - .... . ....... .-.. .- --.. -.-- ....... -.. ---" +
-			" --. .-.-.-"
-			)
+		self.assertEqual(self.cipher_binary.cipher(),
+			"1010011 1101100 1101001 1101101 100001")
 		self.assertEqual(self.cipher_caesar.cipher(),  
 			"Vjg swkem dtqyp hqz lworu qxgt vjg ncba fqi.")
+		self.assertEqual(self.cipher_morse.cipher(), 
+			"... .-.. .. -- -.-.-- ")
+		self.assertEqual(self.cipher_pig.cipher(), 
+			"heTay uickqay rownbay oxfay umpsjay overyay hetay azylay ogday.")
+
+
+	def test_decipher(self):
+		self.assertEqual(
+			binary.Binary("1010011 1101100 1101001 1101101 100001").decipher(), 
+			self.short_text
+		)
+		self.assertEqual(
+			caesar_cipher.CaesarCipher(
+				"Vjg swkem dtqyp hqz lworu qxgt vjg ncba fqi.", 
+				self.caesar_key).decipher(), 
+			self.long_text
+		)
+		self.assertEqual(
+			morse_code.MorseCode("... .-.. .. -- -.-.-- ").decipher(),
+			self.short_text.upper(),
+		)
+		self.assertEqual(
+			pig_latin.PigLatin((
+				"heTay uickqay rownbay oxfay umpsjay overyay hetay"
+				" azylay ogday.")).decipher(),
+			self.long_text,
+		)
